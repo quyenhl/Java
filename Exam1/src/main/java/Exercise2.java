3b2e59ac22c132c682ab6e8dad031292d78e8724
Hoàng Lệ Quyên (1M-Consumer-KCN-MB) <quyenhl@vingroup.net>
2021-06-15 17:09:46 +0700

    Exam1

--- Exam1/src/main/java/Exercise2.java
+++ Exam1/src/main/java/Exercise2.java
@@ -2,45 +2,50 @@ import java.util.Scanner;
 
 public class Exercise2 {
     public  void enterPwd(){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter Password: ");
+//        String pwd = sc.nextLine();
+//
+////       isValidPassword(pwd);
+//        System.out.println(isValidPassword(pwd));
+        boolean isComplete =false;
+        while (!isComplete) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter Password: ");
-        String pwd = sc.nextLine();
-
-//       isValidPassword(pwd);
-        System.out.println(isValidPassword(pwd));
+         System.out.println("enter password:");
+                String pwd = sc.nextLine();
+                isValidPassword(pwd);}
+        isComplete = true;
 
 
     }
-    public static String isValidPassword(String password)
+
+    public static void isValidPassword(String password)
     {
-        String result = "Valid Password";
+        Scanner sc = new Scanner(System.in);
+
+        String upperCaseChars = "(.*[A-Z].*)";
+        String numbers = "(.*[0-9].*)";
+        String specialChars = "(.*[@,#,$,%,_,-].*$)";
 
         if (password.length() < 10)
         {
             System.out.println("Mật khẩu yếu. Phải lớn hơn 10 ký  tự");
 
-        }
-        String upperCaseChars = "(.*[A-Z].*)";
-        if (!password.matches(upperCaseChars ))
-        {
+
+        } else if (!password.matches(upperCaseChars )){
+
             System.out.println("Mật khẩu chưa đủ mạnh. Phải có ít nhất 1 chữ hoa");
 
-        }
+        } else if (!password.matches(numbers )){
 
-        String numbers = "(.*[0-9].*)";
-        if (!password.matches(numbers ))
-        {
             System.out.println("Mật khẩu chưa đủ mạnh. Phải có ít nhất 1 số");
 
-        }
-        String specialChars = "(.*[@,#,$,%,_,-].*$)";
-        if (!password.matches(specialChars ))
-        {
+        } else if (!password.matches(specialChars )){
             System.out.println("Mật khẩu chưa đủ mạnh. Phải chứ kí tự đặc biệt.");
 
-        }
-        return (result);
+        } else {System.out.println("Valid password");}
     }
+
 
 
 }
--- Exam1/src/main/java/Main.java
+++ Exam1/src/main/java/Main.java
@@ -3,10 +3,10 @@ public class Main {
        //Excercise1 ex1 = new Excercise1();
        //ex1.excercise1();
 
-       // Exercise2 ex2 = new Exercise2();
-       // ex2.enterPwd();
-        Exercise3 ex3 = new Exercise3();
-        ex3.enterMonth();
+       Exercise2 ex2 = new Exercise2();
+       ex2.enterPwd();
+        //Exercise3 ex3 = new Exercise3();
+        //ex3.enterMonth();
     }
 
 }
