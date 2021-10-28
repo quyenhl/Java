1d9fcb4e7a48ab6f35dcfdb3beee485ec6b98010
Lệ Quyên <quyen.hoang@onemount.com>
2021-10-28 17:48:03 +0700

    finaltest

--- FinalJava/FinalExamB/src/main/java/ListStudent.java
+++ FinalJava/FinalExamB/src/main/java/ListStudent.java
@@ -11,24 +11,24 @@ public class ListStudent
 {
     public static List<Student> createListStudent() {
         List<Student> students = new ArrayList<>();
-        students.add(new Student(18424005, "Hoàng Mai Ngọc", "hn333@gmail.com", 1, 0, 0, 1.5));
-        students.add(new Student(18424013, "Hoàng Mai Hoa", "hn33333@ogmail.com", 3, 8, 1, 2.5));
-        students.add(new Student(18424014, "Hoàng Mai Lan", "hn3332@gmail.com", 4, 3, 2, 3));
-        students.add(new Student(18424015, "Hoàng Mai Huệ", "hn1@gmail.com", 1, 2, 2, 4));
-        students.add(new Student(18424016, "Hoàng Mai Nam", "hn3@gmail.com", 4, 4, 3, 5.5));
-        students.add(new Student(18424017, "Hoàng Mai Ba", "hn4@gmail.com", 9, 9, 4, 7));
-        students.add(new Student(18424018, "Hoàng Mai Ca", "hn5@gmail.com", 8, 4, 5, 4));
-        students.add(new Student(18424019, "Hoàng Mai Da", "hn6@gmail.com", 9, 1, 6, 8));
-        students.add(new Student(18424010, "Hoàng Mai En", "hn7@gmail.com", 0, 9, 7, 1.5));
-        students.add(new Student(18424011, "Hoàng Mai Phuong", "hn8@gmail.com", 1, 3, 8, 8.5));
-        students.add(new Student(18424100, "Hoàng Mai Suong", "hn9@gmail.com", 2, 4, 9, 3.5));
-        students.add(new Student(18424102, "Hoàng Mai Hiên", "hn10@gmail.com", 3, 2, 10, 4));
-        students.add(new Student(18424103, "Hoàng Mai  Ly", "hn11@gmail.com", 0, 9, 9, 8.5));
-        students.add(new Student(18424104, "Hoàng Mai Mi", "hn12@gmail.com", 5, 1, 9, 1));
-        students.add(new Student(18424105, "Hoàng Mai Ni", "hn13@gmail.com", 7, 9, 8, 0));
-        students.add(new Student(18424106, "Hoàng Mai Ong", "hn14@gmail.com", 9, 2, 7, 8));
-        students.add(new Student(18424107, "Hoàng Mai Uyen", "hn16@gmail.com", 9, 9, 6, 1.5));
-        students.add(new Student(18424108, "Hoàng Mai Tra", "hn18@gmail.com", 0, 3, 5, 8));
+        students.add(new Student(23332001, "Hoàng Mai Ngọc", "hn333@gmail.com", 1, 0, 0, 1.5));
+        students.add(new Student(23332002, "Hoàng Mai Hoa", "hn33333@ogmail.com", 3, 8, 1, 2.5));
+        students.add(new Student(23332003, "Hoàng Mai Lan", "hn3332@gmail.com", 4, 3, 2, 3));
+        students.add(new Student(23332015, "Hoàng Mai Huệ", "hn1@gmail.com", 1, 2, 2, 4));
+        students.add(new Student(23332016, "Hoàng Mai Nam", "hn3@gmail.com", 4, 4, 3, 5.5));
+        students.add(new Student(23332017, "Hoàng Mai Ba", "hn4@gmail.com", 9, 9, 4, 7));
+        students.add(new Student(23332018, "Hoàng Mai Ca", "hn5@gmail.com", 8, 4, 5, 4));
+        students.add(new Student(23332019, "Hoàng Mai Da", "hn6@gmail.com", 9, 1, 6, 8));
+        students.add(new Student(23332010, "Hoàng Mai En", "hn7@gmail.com", 0, 9, 7, 1.5));
+        students.add(new Student(23332011, "Hoàng Mai Phuong", "hn8@gmail.com", 1, 3, 8, 8.5));
+        students.add(new Student(23332100, "Hoàng Mai Suong", "hn9@gmail.com", 2, 4, 9, 3.5));
+        students.add(new Student(23332102, "Hoàng Mai Hiên", "hn10@gmail.com", 3, 2, 10, 4));
+        students.add(new Student(23332103, "Hoàng Mai  Ly", "hn11@gmail.com", 0, 9, 9, 8.5));
+        students.add(new Student(23332104, "Hoàng Mai Mi", "hn12@gmail.com", 9, 9, 9, 9));
+        students.add(new Student(23332105, "Hoàng Mai Ni", "hn13@gmail.com", 7, 9, 8, 0));
+        students.add(new Student(23332106, "Hoàng Mai Ong", "hn14@gmail.com", 9, 9, 7, 8));
+        students.add(new Student(23332107, "Hoàng Mai Uyen", "hn16@gmail.com", 9, 9, 6, 1.5));
+        students.add(new Student(23332108, "Hoàng Mai Tra", "hn18@gmail.com", 0, 3, 5, 8));
 
         students.forEach(user -> {
             System.out.println("ID : " + user.getId() + ", Name : " + user.getName() + ", Email : " + user.getEmail() + ", bonus : " + user.getBonus()
